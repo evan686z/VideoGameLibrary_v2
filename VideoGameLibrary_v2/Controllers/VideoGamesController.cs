084836e31e0479161bec9ecf4467562ac9ca11e5
evan686z <friise@mail.nmc.edu>
2018-03-16 15:07:28 -0400

    Updated VideoGameController: added sort ability, added search ability, added filter by year ability. Updated Create, Delete, Details, Edit, and Index views for the VideoGameController.

--- VideoGameLibrary_v2/Controllers/VideoGamesController.cs
+++ VideoGameLibrary_v2/Controllers/VideoGamesController.cs
@@ -14,10 +14,82 @@ namespace VideoGameLibrary_v2.Controllers
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: VideoGames
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string sortOrder)
         {
-            return View(db.VideoGames.ToList());
+            ApplicationDbContext dbLocal = new ApplicationDbContext();
+
+            // create a distinct list of years for the years filter
+            ViewBag.Years = ListOfYears();
+
+            // return the data context as an enumerable
+            IEnumerable<VideoGame> videoGames;
+            using (dbLocal)
+            {
+                videoGames = dbLocal.VideoGames.ToList() as IList<VideoGame>;
+            }
+
+            // sort by name unless posted as a new sort
+            // *Note* don't need .ToList() ?... videoGames = videoGames.ToList().OrderBy(v => v.ReleaseDate);
+            switch (sortOrder)
+            {
+                case "ReleaseDate":
+                    videoGames = videoGames.OrderBy(v => v.ReleaseYear);
+                    break;
+                default:
+                    videoGames = videoGames.OrderBy(v => v.Name);
+                    break;
+            }
+
+            return View(videoGames);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string searchCriteria, string yearFilter)
+        {
+            ApplicationDbContext dbLocal = new ApplicationDbContext();
+
+            // create a distinct list of years for the years filter
+            ViewBag.Years = ListOfYears();
+
+            // return the data context as an enumerable
+            IEnumerable<VideoGame> videoGames;
+            using (dbLocal)
+            {
+                videoGames = dbLocal.VideoGames.ToList() as IList<VideoGame>;
+            }
+
+            // if posted with a search on video game name
+            if (searchCriteria != null)
+            {
+                videoGames = videoGames.Where(v => v.Name.ToUpper().Contains(searchCriteria.ToUpper()));
+            }
+
+            // if posted with a filter on release year
+            if (yearFilter != "" || yearFilter == null)
+            {
+                videoGames = videoGames.Where(v => v.ReleaseYear == yearFilter);
+            }
+
+            return View(videoGames);
+        }
+
+        [NonAction]
+        private IEnumerable<string> ListOfYears()
+        {
+            ApplicationDbContext dbLocal = new ApplicationDbContext();
+
+            // return the data context as an enumerable
+            IEnumerable<VideoGame> videoGames;
+            using (dbLocal)
+            {
+                videoGames = dbLocal.VideoGames.ToList() as IList<VideoGame>;
+            }
+
+            // get a distinct list of years
+            var years = videoGames.Select(v => v.ReleaseYear).Distinct().OrderBy(x => x);
+
+            return years;
         }
 
         // GET: VideoGames/Details/5
