7ddcf9166088df974da6788284381e3dc35cac37
evan686z <friise@mail.nmc.edu>
2018-03-15 21:49:03 -0400

    Add project files.

---
+++ VideoGameLibrary_v2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VideoGameLibrary_v2.Startup))]
+namespace VideoGameLibrary_v2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
